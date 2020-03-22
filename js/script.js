d8eae2743892eb79a029c1b734856df808c1d392
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-03-22 18:23:57 +0300

    add second

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ for (var i = 0; i < 4; i++) {
 };
 
 document.getElementById('out').innerHTML = out;
+
+var out2 = '';
+var q2 = 4;
+var r2 = 10;
+
+for (var i = 0; i < 4; i++) {
+    for (var k = 0; k < 6; k++) {
+        if (k < q2 || k > r2) {
+            // out2 += '_';
+            out2 += '&nbsp;&nbsp;';
+        } else {
+            out2 += '* ';
+        }  
+    };
+    q2--;
+    r2++;
+    out2 += '<br>';
+};
+
+document.getElementById('out2').innerHTML = out2;
